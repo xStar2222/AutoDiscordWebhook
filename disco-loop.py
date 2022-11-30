8263f6f5f52f56b58b33b61e0278a92ce3374ff6
Arthur Casey <57399280+xStar2222@users.noreply.github.com>
2022-11-29 22:43:46 -0800

    Add files via upload

---
+++ disco-loop.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import json
+import time
+
+with open("settings.json") as f:
+    settings = json.load(f)
+
+def main():
+    webhook = DiscordWebhook(url=settings['discordWebhook'])
+    embed = DiscordEmbed()
+    embed.set_title(f"TITLE")
+    embed.set_description(f"DESCRIPTION")
+    embed.set_timestamp()
+    embed.set_color(0xFFFFFF)
+
+    webhook.add_embed(embed)
+    responce = webhook.execute()
+    return
+
+while True:
+    print("Starting Loop")
+    main()
+    time.sleep(10)
+    
